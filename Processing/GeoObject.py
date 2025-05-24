c5814eb229595b6af62983eeff56912944330f52
Michał Zagajewski <85062220+m-zagajewski@users.noreply.github.com>
2025-05-24 14:26:19 +0200

    Wstępna analiza danych z map

--- Processing/GeoObject.py
+++ Processing/GeoObject.py
@@ -1,11 +1,18 @@
+from typing import Optional, Dict, Any
+
 import geopandas as gpd
 import pandas as pd
 import pyogrio.errors
+import numpy as np
+
+from shapely.geometry import Point
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 class GeoObject:
+        gdf: gpd.GeoDataFrame
+
         def __init__(self, path):
                 try:
                         self.gdf = gpd.read_file(path)
@@ -13,12 +20,53 @@ class GeoObject:
                         print(f"Could not find file: {path}")
                         exit(0)
 
+                # Convert DataFrame to meters notation
+                # self.gdf = self.gdf.to_crs(epsg=3857)
+                self.process()
                 print(self.gdf.head())
 
+        def process(self):
+                self.gdf['center'] = self.gdf['geometry'].centroid
 
-def test():     
-        GeoObject("../data/osm/mazowieckie-latest-free/gis_osm_buildings_a_free_1.shp")
+        def count_objects(self, center: Point, radius: float, attr_filters: Optional[Dict[str, Any]] = None) -> int:
+                # reference_point = gpd.GeoSeries([center], crs='EPSG:4326').to_crs(self.gdf.crs).iloc[0]
+                # self.gdf['distance'] = self.gdf.geometry.distance(reference_point)
+
+                self.gdf['distance'] = GeoObject.haversine(center.x, center.y,
+                                                                self.gdf['center'].x, self.gdf['center'].y)
+
+                print(self.gdf.head())
+                filtered_gdf = self.gdf[self.gdf['distance'] <= radius]
+
+                if attr_filters is not None:
+                        for key, value in attr_filters.items():
+                                filtered_gdf = filtered_gdf[filtered_gdf[key] == value]
+
+                print("Length: ", len(filtered_gdf))
+                print(filtered_gdf.head())
+                #print(filtered_gdf)
+                return len(filtered_gdf)
+
+        @staticmethod
+        def haversine(lat1, lon1, lat2, lon2):
+                R = 6371000  # Earth radius in meters
+                phi1 = np.radians(lat1)
+                phi2 = np.radians(lat2)
+                delta_phi = np.radians(lat2 - lat1)
+                delta_lambda = np.radians(lon2 - lon1)
+
+                a = np.sin(delta_phi / 2.0) ** 2 + \
+                np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda / 2.0) ** 2
+
+                c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+                return R * c  # Distance in meters
+
+
+def test2():
+        print("licze dla leszna")
+        gobject = GeoObject("../data/osm/mazowieckie-latest-free/gis_osm_pois_a_free_1.shp")
+        center = Point(20.591443, 52.257846)
+        gobject.count_objects(center, 1000)
 
 if __name__ == "__main__":
-        test()
-        
+        test2()
--- Processing/Province.py
+++ Processing/Province.py
@@ -1,22 +1,65 @@
 import os
 from pathlib import Path
-from typing import List
+from typing import List, Dict, Optional
+
+from geopandas import GeoDataFrame
 
 from Processing.GeoObject import GeoObject
 
+from shapely.geometry import Point
+from geopy.geocoders import Nominatim
+import geopandas as gpd
 
 class Province:
         path: str
+        center: Point
+        radius: float
         files: List[str]
-        objects: List[object]
+        gpd: GeoDataFrame
+        geo_object_dict: Dict[str, GeoObject]
 
-        def __init__(self, path):
-                self.path = path
+        def __init__(self, province:str, center: Point, radius: float):
+                self.path = f"../data/osm/{province}-latest-free"
+                self.center = center
+                self.radius = radius
+                self.find_files()
+                self.geo_object_dict = dict()
 
         def find_files(self):
                 directory = Path(self.path)
                 self.files = [file.name for file in directory.iterdir() if file.suffix == '.shp']
 
-        def process_files(self):
-                for file in self.files:
-                        geo = GeoObject(file)
+        # @staticmethod
+        # def find_province(point: Point)->str | None:
+        #         geolocator = Nominatim(user_agent="province_locator")
+        #         location = geolocator.reverse((point.y, point.x), exactly_one=True, language='en')
+        #
+        #         if location and 'address' in location.raw:
+        #                 province = location.get('state')
+        #                 print("Found province : ", province)
+        #                 return province
+        #         else:
+        #                 print("Could not find province")
+        #                 return None
+        def get_geo_object(self, file):
+                if self.geo_object_dict.get(file) is None :
+                        self.geo_object_dict[file] = GeoObject(file)
+
+                return self.geo_object_dict[file]
+
+        def find_count(self, file, fclass: Optional[str]) -> int:
+                geo_object = self.get_geo_object(file)
+                res = geo_object.count_objects(self.center, self.radius, {'fclass': fclass} if fclass else None)
+                return res
+
+        def building_count(self):
+                return self.find_count(file=f"{self.path}/gis_osm_buildings_a_free_1.shp", fclass=None)
+        
+
+def test():
+        province = Province('mazowieckie', Point(21.08862, 52.227601), 5000)
+        print("Building count = " + str(province.building_count()))
+
+if __name__ == "__main__":
+        test()
+
